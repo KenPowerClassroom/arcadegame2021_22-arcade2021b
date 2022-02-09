1ec0572477e89467e72249944b9c06f151ef9fa4
(Student) - Benjamin Price <C00260297@itcarlow.ie>
2022-02-09 15:00:34 +0000

    Player controller tests started

--- Asteroids-Test/test.cpp
+++ Asteroids-Test/test.cpp
@@ -1,6 +1,18 @@
-#include "pch.h"
+#include"pch.h"
+#include"../Asteroids/PlayerController.h"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(PlayerController, RotationWrapsAroundLeft) {
+	PlayerController p;
+
+	for (int i = 0; i < 361; i++)
+	{
+		p.rotateLeft();
+	}
+
+	EXPECT_EQ(p.returnRotation(), 1);
 }
